5170511f5d0acda338c17fa1107494390f13786d
spacecreatedd <kimzhenya1313@mail.ru>
2024-03-13 11:58:20 +0700

    final ver vendor dir bag fix

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spacecreatedd/server',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ca48b8fa0573813fe7376d1520ed88159bc1d057',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spacecreatedd/server' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ca48b8fa0573813fe7376d1520ed88159bc1d057',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
